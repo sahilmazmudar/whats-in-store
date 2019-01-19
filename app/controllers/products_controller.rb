d84e056895fa6a36183f30890eb182cb4a039995
sahilmazmudar <sahil.maz97@gmail.com>
2019-01-19 14:41:40 -0800

    Added specific quantity purchasing. Clean-up. WIP

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,13 +3,10 @@ class ProductsController < ApplicationController
 
 	# GET /products
 	def index
-		if params[:in_stock] == "true"
-			@products = Product.in_stock
-		else
-			@products = Product.all
-		end
+		params[:in_stock] == "true" ? @products = Product.in_stock : @products = Product.all
 		respond_to do |format|
-			format.json {render json: @products}
+			response = {status: 200, message: "Success", products: @products}
+			format.json {render json: response}
 		end
 	end
 
@@ -17,13 +14,39 @@ class ProductsController < ApplicationController
 	def show
 		@product = Product.find(params[:id])
 		respond_to do |format|
-			format.json {render json: @product}
+			response = {status: 200, message: "Success", product: @product}
+			format.json {render json: response}
 		end
 	end
 
 	# PUT /products/:id
 	def update
 		@product = Product.find(params[:id])
-		@product.update(inventory_count: @product.inventory_count - 1)
+		quantity = params.has_key?(:quantity) ? params[:quantity].to_i : 1
+		if @product.inventory_count < 1
+			respond_to do |format|
+				response = {status: 500, message: "Failed. Product out of stock.", product: @products}
+			format.json {render json: response}
+			end
+		elsif @product.inventory_count < quantity
+			respond_to do |format|
+				response = {status: 200, message: "Failed. Inventory less than requested quantity.", product: @product}
+				format.json {render json: response}
+			end
+		else
+			@product.update(inventory_count: @product.inventory_count - quantity)
+			respond_to do |format|
+				response = {status: 200, message: "Success. Inventory for #{@product.title} reduced by #{quantity}.", product: @product}
+				format.json {render json: response}
+			end
+		end
+	end
+
+	# POST /products
+	def create
+		Product.restock
+		respond_to do |format|
+			format.json {render json: @product}
+		end
 	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,21 @@
 class Product < ApplicationRecord
+
 	class << self
 		def in_stock
 			Product.all.select{ |product| product.inventory_count > 0}
+		end
+
+		def restock
+			Product
+			i = 0
+			4.times do
+				random_title = ('a'..'z').to_a.shuffle[0,8].join
+				random_price = rand 2..500
+				random_inventory = rand 2..50
+				product = Product.create(title: "Product" + random_title,
+										 price: random_price,
+										 inventory_count: random_inventory)
+			end
 		end
 	end
 end
